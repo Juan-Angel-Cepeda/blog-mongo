264f67e4ad317eaa82f7beefd291ecf599eee186
angelcepeda <jangelcepeda@protonmail.com>
2023-05-07 13:28:58 -0700

    cambio home

--- Home.py
+++ Home.py
@@ -21,4 +21,4 @@ with col3:
 st.title("Admin de Blog Mongo DB")
 st.markdown("## 338832 Juan Angel Cepeda Fernandez")
 st.markdown("### Bases de Datos Avanzadas")
-st.markdown("#### Ve a la pagina login para registrar tu usuario")
+st.markdown("#### Ve a la pagina Registrarse para registrar tu usuario")
